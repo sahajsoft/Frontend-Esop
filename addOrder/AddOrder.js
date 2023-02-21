bed5c735d1c7746fac3b655f5cb9e8059e92d334
KCSPreethi <preethik@sahaj.ai>
2023-02-21 17:26:53 +0530

    Add html method to place order

--- addOrder/AddOrder.js
+++ addOrder/AddOrder.js
@@ -1,13 +1,76 @@
-const button = document.getElementById("b1")
-button.onclick = function() {myFunction()};
+const form = document.getElementById("form1");
+const qty = document.getElementById("quantity");
+var type
+const price = document.getElementById("price");
+var esopType= document.getElementById("esopType")
+console.log("hi")
 
-function myFunction() {
-  console.log( "YOU CLICKED ME!");
-  fetch('http://localhost:8080/user/atul_1/order', {
-      			method: 'GET',
-      		})
-      		.then(response => response.json())
-      		.then(response => {
-      			console.log(response)
-      		})
+
+function buySellCheck()
+{
+    if (document.getElementById('buy').checked) {
+        type= "BUY"
+        document.getElementById("esopType").disabled= true
+
+    }
+    else {
+        type= "SELL"
+         document.getElementById("esopType").disabled= false
+        document.getElementById("esopType").checked = "NORMAL"
+
+    }
+
 }
+
+
+form.addEventListener("submit", function(event) {
+	event.preventDefault();
+    var content
+
+    if(type=="SELL")
+    {
+        content = JSON.stringify({
+                        quantity: parseInt(qty.value),
+                        type: type,
+                        price: parseInt(price.value),
+                        esopType: esopType.value
+                    })
+     }
+     else
+     {
+           content = JSON.stringify({
+                             quantity: parseInt(qty.value),
+                             type: type,
+                             price: parseInt(price.value)
+                         })
+      }
+
+	fetch("http://localhost:8080/user/atul_1/order", {
+			method: "POST",
+			headers: {
+                "Content-type": "application/json; charset=UTF-8"
+              },
+			body:
+				content
+
+		})
+		.then((response) => response.json())
+		.then((json) =>{
+			console.log(json);
+			if(json.error)
+			{
+			document.getElementById("message").innerHTML = json.error
+			document.getElementById("message").style.color="red";
+			}
+			else
+			{
+			document.getElementById("message").style.color="green";
+			document.getElementById("message").innerHTML = "Order Placed";
+            document.getElementById("esopType").disabled= false
+			}
+
+			}
+		)
+
+});
+
